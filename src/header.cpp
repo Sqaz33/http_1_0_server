2a7b06c0f50f80c924e42c757ccbc07071e996ba
Matveev Stepan <mset321@gmail.com>
2026-02-03 12:53:15 +0300

    feat: add new header values and names

--- src/header.cpp
+++ src/header.cpp
@@ -4,7 +4,10 @@ namespace http_server {
 
 const char* Header::NAME_CONTENT_LEN = "Content-Length";
 const char* Header::NAME_CONTENT_TYPE =  "Content-Type";
+const char* Header::NAME_ACCEPT =  "Accept";
 
 const char* Header::VALUE_TEXT_HTML = "text/html";
+const char* Header::VALUE_APPLICATION_JSON = "application/json";
+const char* Header::VALUE_APPLICATION_JSON_UTF8 = "application/json; charset=utf-8";
 
 } // namespace http_server
--- src/header.hpp
+++ src/header.hpp
@@ -10,8 +10,11 @@ struct Header {
 
     static const char* NAME_CONTENT_LEN;
     static const char* NAME_CONTENT_TYPE;
+    static const char* NAME_ACCEPT;
 
     static const char* VALUE_TEXT_HTML; 
+    static const char* VALUE_APPLICATION_JSON;
+    static const char* VALUE_APPLICATION_JSON_UTF8;
 };
 
 }
